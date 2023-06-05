23cd0a05b54542bd16a55bdfe6e510b470c4261d
lorenzoaniel <lorenzoaniel@gmail.com>
2023-06-04 19:16:56 -0600

    initial setup and barebones rough

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+import React from "react";
+
+const Menu = () => {
+	return (
+		<section
+			className={clsx("menu", "h-[42.9rem] w-[32.7rem] bg-black self-center justify-self-center")}
+		>
+			Menu
+		</section>
+	);
+};
+
+export default Menu;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					btn: {
+						100: "#31C3BD",
+						200: "#65E9E4",
+					},
+					bg: {
+						100: "#1A2A33",
+						200: "#1F3641",
+					},
+				},
+				secondary: {
+					btn: {
+						100: "#F2B137",
+						200: "#FFC860",
+						300: "#A8BFC9",
+						400: "#DBE8ED",
+					},
+				},
+			},
+			fontFamily: {
+				default: ["outfit", "mono"],
+			},
+		},
+	},
+	plugins: [],
+};
