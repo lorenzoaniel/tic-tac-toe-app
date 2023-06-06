c96a6885ae940537d7ebc9fe3fd6e96811121a62
lorenzoaniel <lorenzoaniel@gmail.com>
2023-06-05 18:41:55 -0600

    Menu: responsive sizing

---
+++ src/components/menu/Menu.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Menu from ".";
+
+const meta: Meta<typeof Menu> = {
+	title: "components/Menu",
+	component: Menu,
+	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+	tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Menu>;
+
+export const Primary: Story = {};
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -5,7 +5,11 @@ import Logo from "../logo";
 const Menu = () => {
 	return (
 		<section
-			className={clsx("menu", "h-[42.9rem] w-[32.7rem] bg-black self-center justify-self-center")}
+			className={clsx(
+				"menu",
+				"h-[42.9rem] w-[32.7rem] bg-black self-center justify-self-center",
+				"md:h-[47.1rem] md:w-[46rem]"
+			)}
 		>
 			<Logo />
 		</section>
