ddca8a3dae23d0bfaa86c5a5d84ebace1f3346b3
lorenzoaniel <lorenzoaniel@gmail.com>
2023-06-05 21:15:06 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,10 +25,23 @@ module.exports = {
 						300: "#A8BFC9",
 						400: "#DBE8ED",
 					},
+					text: {
+						100: "#F2B137",
+						200: "#FFC860",
+						300: "#A8BFC9",
+						400: "#DBE8ED",
+					},
 				},
 			},
 			fontFamily: {
 				default: ["outfit", "mono"],
+			},
+			fontSize: {
+				"heading-l": ["4rem", { letterSpacing: "0.25rem", lineHeight: "5rem", fontWeight: 700 }],
+				"heading-m": ["2.4rem", { letterSpacing: "0.15rem", lineHeight: "3rem", fontWeight: 700 }],
+				"heading-s": ["2rem", { letterSpacing: "0.125rem", lineHeight: "2.5rem", fontWeight: 700 }],
+				"heading-xs": ["1.6rem", { letterSpacing: "0.1rem", lineHeight: "2rem", fontWeight: 700 }],
+				body: ["1.4rem", { letterSpacing: "0.0875rem", lineHeight: "1.8rem", fontWeight: 500 }],
 			},
 			screens: {
 				lg: "1440px",
