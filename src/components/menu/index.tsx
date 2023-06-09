3aa30946787d0e84dca51a5cadb1da19fe793625
lorenzoaniel <lorenzoaniel@gmail.com>
2023-06-08 19:48:28 -0600

    menu: added 3 components, menu is now completed just needs state

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,17 +1,39 @@
 import clsx from "clsx";
 import React from "react";
 import Logo from "../logo";
+import PlayerMark from "../playermark";
+import DefaultBtn from "../button/defaultbtn";
 
 const Menu: React.FC = () => {
+	//TODO: add statemanagement no Props for this one
 	return (
 		<section
 			className={clsx(
 				"menu",
-				"h-[42.9rem] w-[32.7rem] bg-black self-center justify-self-center",
+				"h-[42.9rem] w-[32.7rem]",
+				"bg-transparent",
+				"flex flex-col items-center justify-between",
 				"md:h-[47.1rem] md:w-[46rem]"
 			)}
 		>
 			<Logo />
+			<PlayerMark markToggleActive={false} />
+			<DefaultBtn
+				btntype={"newgame"}
+				isPrimary={false}
+				title={"NEW GAME (VS CPU)"}
+				onClick={function () {
+					throw new Error("Function not implemented.");
+				}}
+			/>
+			<DefaultBtn
+				btntype={"newgame"}
+				isPrimary={true}
+				title={"NEW GAME (VS PLAYER)"}
+				onClick={function () {
+					throw new Error("Function not implemented.");
+				}}
+			/>
 		</section>
 	);
 };
