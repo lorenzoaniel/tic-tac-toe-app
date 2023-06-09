6e37567b2a6658f565cdd8fed6a454e2e9ab1843
lorenzoaniel <lorenzoaniel@gmail.com>
2023-06-08 19:31:31 -0600

    playermark: added reminder

--- src/components/playermark/index.tsx
+++ src/components/playermark/index.tsx
@@ -1,8 +1,14 @@
 import clsx from "clsx";
 import React from "react";
+import MarkToggleBtn from "../button/marktoggle";
 
-const PlayerMark: React.FC = () => {
+interface Props {
+	markToggleActive: boolean;
+}
+
+const PlayerMark: React.FC<Props> = ({ markToggleActive }) => {
 	const title = "PICK PLAYER 1'S MARK";
+	const reminder = "REMEMBER : X GOES FIRST";
 
 	return (
 		<div
@@ -10,13 +16,17 @@ const PlayerMark: React.FC = () => {
 				"player-mark",
 				"w-[32.7rem] h-[20.5rem] rounded-[1.5rem] p-[2.4rem]",
 				"bg-primary-bg-200",
-				"flex flex-col items-center justify-center",
+				"flex flex-col items-center justify-around",
 				"md:w-[46rem]"
 			)}
 		>
 			<h1 className={clsx("player-mark-title", "text-heading-xs text-secondary-text-300")}>
 				{title}
 			</h1>
+			<MarkToggleBtn markToggleActive={markToggleActive} />
+			<h2 className={clsx("player-mark-title", "text-body text-secondary-text-300 opacity-50")}>
+				{reminder}
+			</h2>
 		</div>
 	);
 };
