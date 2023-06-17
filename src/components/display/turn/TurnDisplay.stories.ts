fc38427c133f9653eb073c5d1eddcb40c983fab3
lorenzoaniel <lorenzoaniel@gmail.com>
2023-06-16 18:31:51 -0600

    TurnDisplay.stories.ts: added controls

--- src/components/display/turn/TurnDisplay.stories.ts
+++ src/components/display/turn/TurnDisplay.stories.ts
@@ -4,10 +4,21 @@ import TurnDisplay from ".";
 const meta: Meta<typeof TurnDisplay> = {
 	title: "components/displays/TurnDisplay",
 	component: TurnDisplay,
+	argTypes: {
+		isTurnX: {
+			control: {
+				type: "boolean",
+			},
+		},
+	},
 	tags: ["docs"],
 };
 
 export default meta;
 type Story = StoryObj<typeof TurnDisplay>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+	args: {
+		isTurnX: true,
+	},
+};
