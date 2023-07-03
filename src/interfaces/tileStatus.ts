b09aba1112c1909c75c222d794812c16903d918c
lorenzoaniel <lorenzoaniel@gmail.com>
2023-07-02 19:23:15 -0600

    Update tileStatus.ts

--- src/interfaces/tileStatus.ts
+++ src/interfaces/tileStatus.ts
@@ -1,5 +1,3 @@
-import type PlayerIdentity from "@/interfaces/playeridentity";
-
 export default interface TileStatus {
 	isMarkSelected: boolean; //this represents if this tiles X or O mark has been clicked
 	tileID: number;
