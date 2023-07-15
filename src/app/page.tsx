fff8751b335b0ea6cb8ccb413a10d69d283f0997
lorenzoaniel <lorenzoaniel@gmail.com>
2023-07-14 20:11:56 -0600

    centered both Board and Menu child components
    made sure to look into

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
-// import Image from "next/image";
-import Menu from "@/components/menu";
-import clsx from "clsx";
-// import { Inter } from "next/font/google";
+"use client";
 
-// const inter = Inter({ subsets: ["latin"] });
+import Board from "@/components/board";
+import Menu from "@/components/menu";
+import { Store } from "@/interfaces/store";
+import { useStore } from "@/state/useStore";
+import clsx from "clsx";
 
 export default function Home() {
+	const menuState = useStore((state: Store) => state.mainData.menu);
+	console.log(menuState);
+
 	return (
-		<main className={clsx("app", "h-full overflow-y-scroll grid bg-primary-bg-100 py-[18%]")}>
-			<Menu />
+		<main
+			className={clsx(
+				"app",
+				`
+					h-full
+					overflow-y-scroll
+					grid items-center justify-items-center
+					bg-primary-bg-100
+					py-[18%]
+				`
+			)}
+		>
+			{menuState ? <Menu /> : <Board />}
 		</main>
 	);
 }
