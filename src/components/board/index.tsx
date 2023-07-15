e59380e7e122b26ee47ab9ffc341d410c6999685
lorenzoaniel <lorenzoaniel@gmail.com>
2023-07-14 20:51:49 -0600

    Added Dispatches (restart), Selectors (gameModalState, scoreState)
    restart: resets to defaultData for everything, essentially takes you back to menu
    
    gameModalState: type of modal that pops up
    scoreState: scores used in the 3 StatDisplay components at bottom of the board

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -7,8 +7,18 @@ import TurnDisplay from "../display/turn";
 import Restart from "../button/restart";
 import Tile from "../tile";
 import StatDisplay from "../display/stat";
+import { useStore } from "@/state/useStore";
+import { Store } from "@/interfaces/store";
+import Modal from "../modal";
+// import { ModalActiveStatus } from "@/interfaces/modalActiveStatus";
 
 const Board: React.FC = () => {
+	//SELECTORS
+	let selector = {
+		gameModalState: useStore((state: Store) => state.mainData.gameModal),
+		scoreState: useStore((state: Store) => state.mainData.score),
+	};
+
 	//TODO: replace with state
 	const dummyDataTile = {
 		isMarkSelected: false,
@@ -34,6 +44,11 @@ const Board: React.FC = () => {
 			playercpu: true,
 		},
 	};
+	//DISPATCH
+
+	let dispatch = {
+		handleRestart: useStore((state: Store) => state.setModalType),
+	};
 
 	//TODO: replace with state data
 	const createTiles = () => {
@@ -50,28 +65,45 @@ const Board: React.FC = () => {
 	};
 
 	return (
-		<section
-			className={clsx(
-				"board",
-				`
+		<>
+			<section
+				className={clsx(
+					"board",
+					`
           h-[51.6rem] w-[32.8rem]
           grid grid-cols-3 grid-flow-rows
           justify-items-center items-center
 
           md:h-[62.3rem] md:w-[46rem]
         `
-			)}
-		>
-			<Logo variant="board" />
-			<TurnDisplay isTurnX={false} />
-			<Restart />
+				)}
+			>
+				<Logo variant="board" />
+				<TurnDisplay isTurnX={false} />
+				<Restart handleClick={() => dispatch.handleRestart("restartActive", true)} />
 
-			{createTiles()}
+				{createTiles()}
 
-			<StatDisplay playerIdentity={dummyDataStat.p1X} score={14} isTie={false} />
-			<StatDisplay playerIdentity={dummyDataStat.ties} score={32} isTie={true} />
-			<StatDisplay playerIdentity={dummyDataStat.pCpuX} score={11} isTie={false} />
-		</section>
+				<StatDisplay
+					playerIdentity={dummyDataStat.p1X}
+					score={selector.scoreState.player1}
+					isTie={false}
+				/>
+				<StatDisplay
+					playerIdentity={dummyDataStat.ties}
+					score={selector.scoreState.ties}
+					isTie={true}
+				/>
+				<StatDisplay
+					playerIdentity={dummyDataStat.pCpuX}
+					score={selector.scoreState.opponent}
+					isTie={false}
+				/>
+				{Object.values(selector.gameModalState).some((value) => value === true) && (
+					<Modal modalActiveStatus={selector.gameModalState} />
+				)}
+			</section>
+		</>
 	);
 };
 
