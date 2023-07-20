36fdc88e200ac3455f835eb4c591039b6ca13f7c
lorenzoaniel <lorenzoaniel@gmail.com>
2023-07-19 19:48:58 -0600

    Create store.ts

---
+++ src/interfaces/store.ts
@@ -0,0 +1,20 @@
+import { MainData } from "./mainData";
+import TileStatus from "./tileStatus";
+
+export interface StoreActions {
+	setTile: (player: "player1" | "opponent", tileStatus: TileStatus) => void;
+	setMenuState: (status: boolean) => void;
+	setModalType: (
+		modalType: "winActive" | "lostActive" | "restartActive" | "tiedActive",
+		status: boolean
+	) => void;
+	setTurn: (turnType: "xTurn" | "oTurn", status: boolean) => void;
+	setOpponentType: (opponentType: "player1" | "player2" | "playercpu", status: boolean) => void;
+	setMarkType: (status: boolean) => void;
+	resetData: () => void;
+	restartGame: () => void;
+}
+
+export interface Store extends StoreActions {
+	mainData: MainData;
+}
